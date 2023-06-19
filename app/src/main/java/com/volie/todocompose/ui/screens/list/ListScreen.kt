9992c3adab196b9dd0193eb64b25c4a026b028b1
volie <harunuyan6@gmail.com>
2023-06-19 15:38:43 +0300

    Added Default List App Bar & themes

--- app/src/main/java/com/volie/todocompose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/volie/todocompose/ui/screens/list/ListScreen.kt
@@ -14,6 +14,7 @@ import com.volie.todocompose.R
 @Composable
 fun ListScreen(navigateToTaskScreen: (Int) -> Unit) {
     Scaffold(
+        topBar = { ListAppBar() },
         content = {},
         floatingActionButton = {
             ListFab(onFabClicked = navigateToTaskScreen)
--- app/src/main/java/com/volie/todocompose/ui/theme/Color.kt
+++ app/src/main/java/com/volie/todocompose/ui/theme/Color.kt
@@ -1,5 +1,7 @@
 package com.volie.todocompose.ui.theme
 
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -7,10 +9,23 @@ val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
+val Purple500 = Color(0xFF6200EE)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LightGray = Color(0xFFFCFCFC)
+val MediumGray = Color(0xFF9C9C9C)
+val DarkGray = Color(0xFF141414)
 
 val LowPriorityColor = Color(0xFF00C980)
 val MediumPriorityColor = Color(0xFFFFC114)
 val HighPriorityColor = Color(0XFFFF4646)
 val NonePriorityColor = Color(0xFFFFFFFF)
+
+val Colors.topAppBarContentColor: Color
+    @Composable
+    get() = if (isLight) Color.White else LightGray
+
+val Colors.topAppBarBackgroundColor: Color
+    @Composable
+    get() = if (isLight) Purple500 else Color.Black
