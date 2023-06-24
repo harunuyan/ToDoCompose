cd5aed68221014ab653bbcb91bd5215b824b3b39
volie <harunuyan6@gmail.com>
2023-06-24 13:59:06 +0300

    Added delete operation for delete task from database

--- app/src/main/java/com/volie/todocompose/ui/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/volie/todocompose/ui/viewmodels/SharedViewModel.kt
@@ -83,6 +83,18 @@ class SharedViewModel @Inject constructor(private val toDoRepository: ToDoReposi
         }
     }
 
+    private fun deleteTask() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val toDoTask = ToDoTask(
+                id = id.value,
+                title = title.value,
+                description = description.value,
+                priority = priority.value
+            )
+            toDoRepository.deleteTask(toDoTask = toDoTask)
+        }
+    }
+
     fun handleDatabaseActions(action: Action) {
         when (action) {
             Action.ADD -> {
@@ -94,7 +106,7 @@ class SharedViewModel @Inject constructor(private val toDoRepository: ToDoReposi
             }
 
             Action.DELETE -> {
-
+                deleteTask()
             }
 
             Action.DELETE_ALL -> {
